aa5b5ac30771e3006b9ecdd5b0dcef27452f9d89
tansharma <tansharma@gmail.com>
2016-05-19 18:14:13 +0100

    Update ccp4URLProvider.py

--- ccp4/ccp4URLProvider.py
+++ ccp4/ccp4URLProvider.py
@@ -3,32 +3,31 @@
 from subprocess import Popen, PIPE
 from autopkglib import Processor, ProcessorError
 
-DEFAULT_BRANCH = "release"
 BASE_URL = "http://series-70.fg.oisin.rc-harwell.ac.uk/update/"
 
 __all__ = ["ccp4URLProvider"]
 
-class ccp4URLProvider(Processor):
-    """Provides a download URL for a CCP$ update.
-    CCP 1 is not supported."""
-    description = __doc__
-    input_variables = {
-        "branch": {
-            "required": False,
-            "description": (
-                "The update branch. One of 'release', 'beta', or 'nightly'. "
-                "In the TM GUI, 'Normal' corresponds to 'release', 'Nightly' = "
-                "'beta'. Defaults to %s" % DEFAULT_BRANCH)
-        }
-    }
-    output_variables = {
-        "url": {
-            "description": "URL to the latest ccp4 tgz.",
-        }
-    }
+#class ccp4URLProvider(Processor):
+#    """Provides a download URL for a CCP$ update.
+#    CCP 1 is not supported."""
+#    description = __doc__
+#    input_variables = {
+#        "branch": {
+#            "required": False,
+#            "description": (
+#                "The update branch. One of 'release', 'beta', or 'nightly'. "
+#                "In the TM GUI, 'Normal' corresponds to 'release', 'Nightly' = "
+#                "'beta'. Defaults to %s" % DEFAULT_BRANCH)
+#        }
+#    }
+#    output_variables = {
+#        "url": {
+#            "description": "URL to the latest ccp4 tgz.",
+#        }
+#    }
 
     def main(self):
-        url = BASE_URL + self.env.get("branch", DEFAULT_BRANCH)
+        url = BASE_URL 
         # Using curl to fetch Location header(s) because urllib/2
         proc = Popen(['/usr/bin/curl', '-ILs', url], stdout=PIPE, stderr=PIPE)
         out, err = proc.communicate()
