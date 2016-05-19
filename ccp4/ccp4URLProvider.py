da5f0636601a31f19b5ac90e87bd57b9c1dca5f2
ts50 <t.sharma@sussex.ac.uk>
2016-05-19 20:13:30 +0100

    edit ccp4urlprovider

--- ccp4/ccp4URLProvider.py
+++ ccp4/ccp4URLProvider.py
@@ -1,37 +1,53 @@
 #!/usr/bin/python
+#
 
-from subprocess import Popen, PIPE
+import re
+import urllib
+import urllib2
+import urlparse
+from distutils.version import LooseVersion
+
 from autopkglib import Processor, ProcessorError
 
-BASE_URL = "http://series-70.fg.oisin.rc-harwell.ac.uk/update/"
+__all__ = ["CCP4URLProvider"]
 
-__all__ = ["ccp4URLProvider"]
+BASE_URL = "http://series-70.fg.oisin.rc-harwell.ac.uk/update/macosx-x86_64/"
 
-class ccp4URLProvider(Processor):
-#    description = __doc__
-     output_variables = {
-        url = BASE_URL    
-     }
+class CCP4URLProvider(Processor):
+    """Provides URL to the latest Firefox release."""
+    description = __doc__
+    input_variables = {
+        "product_name": {
+            "required": False,
+            "description":
+                "Product to fetch URL .",
+        },
+        "base_url": {
+            "required": False,
+            "description": "Default is '%s." % BASE_URL,
+        },
+    }
+    output_variables = {
+        "url": {
+            "description": "URL to the latest CCP4 product release.",
+        },
+    }
 
-def main(self):
-    url = BASE_URL 
-    # Using curl to fetch Location header(s) because urllib/2
-    proc = Popen(['/usr/bin/curl', '-ILs', url], stdout=PIPE, stderr=PIPE)
-    out, err = proc.communicate()
-    parsed_url = None
-    if err:
-        print (err)
-        raise ProcessorError("curl returned an error: %s" % out)
-        for line in out.splitlines():
-            if line.startswith("Location"):
-                parsed_url = line.split()[1]
-        if not parsed_url:
-            raise ProcessorError(
-                "curl didn't find a resolved 'Location' header we can use. "
-                "Full curl output:\n %s" % "\n".join(out.splitlines()))
+    def get_ccp4_url(self, base_url, product_name):
+        # Construct download URL.
+        return base_url % (product_name)
 
-    self.env["url"] = parsed_url
+    def main(self):
+        """Provide a CCP4 download URL"""
+        # Determine product_name,  and base_url.
+        product_name = self.env["product_name"]
+        base_url = self.env.get("base_url", BASE_URL)
+
+        self.env["url"] = self.get_ccp4_url(
+            base_url, product_name)
+        self.output("Found URL %s" % self.env["url"])
+
 
 if __name__ == "__main__":
-    PROCESSOR = ccp4URLProvider()
+    PROCESSOR = CCP4URLProvider()
     PROCESSOR.execute_shell()
